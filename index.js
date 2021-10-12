5d79d463fba40e8e63ab0bc2eb0055377e2b8e8c
Lex Nastin <lexnastin@gmail.com>
2021-10-12 16:13:08 +0100

    Change emoji reactions + add rule Sadly the Waldro emoji doesn't exist at that point so ig we'll just have to remove him. RIP íº¦!

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const discord = require('discord.js');
 const Commands = require('./modules/commands.js');
 const Intents = discord.Intents;
-const { SlashCommandBuilder, SlashCommandStringOption } = require('@discordjs/builders');
+const { SlashCommandBuilder, SlashCommandStringOption, SlashCommandMentionableOption } = require('@discordjs/builders');
 require('dotenv').config();
 
 const new_state = chalk.bgBlueBright.whiteBright;
@@ -22,23 +22,32 @@ function load_commands(commands) {
 client.on('ready', client => {
     console.log(new_state("WaldroBot is ready!"))
     load_commands([
-        new SlashCommandBuilder().setName('request-feature').setDescription('Let\'s say you wanna request a new bot feature... well then do!').addStringOption(new SlashCommandStringOption().setName('command-name').setDescription('Command/feature name. (if not a command, put literally any text in here)').setRequired(true)).addStringOption(new SlashCommandStringOption().setName('description').setDescription('Describe the function of this command/feature.').setRequired(true))
+        new SlashCommandBuilder().setName('request-feature').setDescription('Let\'s say you wanna request a new bot feature... well then do!').addStringOption(new SlashCommandStringOption().setName('command-name').setDescription('Command/feature name. (if not a command, put literally any text in here)').setRequired(true)).addStringOption(new SlashCommandStringOption().setName('description').setDescription('Describe the function of this command/feature.').setRequired(true)),
+        new SlashCommandBuilder().setName('r1').setDescription('Someone misbehaving? Hit them with the "No Spamming" rule!').addMentionableOption(new SlashCommandMentionableOption().setName('mention').setDescription('Set field if you want someone to be mentioned'))
     ]);
 });
 
 client.on('messageCreate', message => {
     try {
-        if (message.content.includes('<:JohnWaldron:893175829675278406>')) {
-            message.react(client.emojis.resolveId('893175829675278406'));
-        }
-        if (message.content.includes('<:GigaWaldron:897158188930060338>')) {
-            message.react(client.emojis.resolveId('897158188930060338'));
+        if (message.content.includes('<:CianPog:897468006869921823>')) {
+            message.react(client.emojis.resolveId('897468006869921823'));
         }
     }
     catch (err) {
         console.log(error(err));
     }
-})
+});
+
+client.on('messageUpdate', message => {
+    try {
+        if (message.content.includes('<:CianPog:897468006869921823>')) {
+            message.react(client.emojis.resolveId('897468006869921823'));
+        }
+    }
+    catch (err) {
+        console.log(error(err));
+    }
+});
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
@@ -58,5 +67,14 @@ client.on('interactionCreate', async interaction => {
 
             await client.users.resolve('383363277100417027').send({embeds: [temp_embed]});
             //await client.users.resolve('383363277100417027').send(`User '@${interaction.user.tag} (${interaction.user.id})' requested feature '${interaction.options.getString('command-name')}' with description '${interaction.options.getString('description')}'`);
+        case 'r1':
+            await interaction.deferReply({ephemeral: true});
+            await interaction.followUp(`Sure! Rule 1 that is!`);
+
+            if (interaction.options.getMentionable('mention')) {
+                interaction.channel.send(`No spamming <@${interaction.options.getMentionable('mention').id}>!`)
+            } else {
+                interaction.channel.send(`No spamming!`)
+            }
     }
-})
+});
