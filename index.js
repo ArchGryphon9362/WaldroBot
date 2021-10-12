4141f8795d12126fb10bb72df8c1ffd41528e7cb
Lex Nastin <lexnastin@gmail.com>
2021-10-12 02:38:06 +0100

    Allowed feature requests! You just do the /request-feature command, and there ya go!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const discord = require('discord.js');
+const Commands = require('./modules/commands.js');
 const Intents = discord.Intents;
+const { SlashCommandBuilder, SlashCommandStringOption } = require('@discordjs/builders');
 require('dotenv').config();
 
 const new_state = chalk.bgBlueBright.whiteBright;
@@ -8,19 +10,20 @@ const error = chalk.bgRed.whiteBright;
 const warn = chalk.bgYellow;
 const log = chalk.green;
 
-// console.log(`
-// Test Colors:
-// ${new_state('new_state')}
-// ${error('error')}
-// ${warn('warn')}
-// ${log('log')}
-// `);
-
 const client = new discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.login(process.env.TOKEN);
 
+function load_commands(commands) {
+    const command_thing = new Commands(process.env.TOKEN, process.env.CLIENT_ID, process.env.GUILD_ID);
+    command_thing.set_commands(commands);
+    command_thing.set_up();
+}
+
 client.on('ready', client => {
     console.log(new_state("WaldroBot is ready!"))
+    load_commands([
+        new SlashCommandBuilder().setName('request-feature').setDescription('Let\'s say you wanna request a new bot feature... well then do!').addStringOption(new SlashCommandStringOption().setName('command-name').setDescription('Command/feature name. (if not a command, put literally any text in here)').setRequired(true)).addStringOption(new SlashCommandStringOption().setName('description').setDescription('Describe the function of this command/feature.').setRequired(true))
+    ]);
 });
 
 client.on('messageCreate', message => {
@@ -34,5 +37,26 @@ client.on('messageCreate', message => {
     }
     catch (err) {
         console.log(error(err));
+    }
+})
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+    switch (commandName) {
+        case 'request-feature':
+            await interaction.deferReply({ephemeral: true});
+            await interaction.followUp(`Sure! Requested command '${interaction.options.getString('command-name')}'!`);
+
+            let temp_embed = new discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setTitle('New Feature Request!')
+                .setAuthor(`${interaction.user.tag} (${interaction.user.id})`, interaction.user.avatarURL())
+                .setDescription(`@${interaction.user.username} requested a feature...`)
+                .addField(interaction.options.getString('command-name'), interaction.options.getString('description'), false);
+
+            await client.users.resolve('383363277100417027').send({embeds: [temp_embed]});
+            //await client.users.resolve('383363277100417027').send(`User '@${interaction.user.tag} (${interaction.user.id})' requested feature '${interaction.options.getString('command-name')}' with description '${interaction.options.getString('description')}'`);
     }
 })
---
+++ modules/commands.js
@@ -0,0 +1,35 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const chalk = require('chalk');
+
+module.exports = class Commands {
+    constructor (token, client_id, guild_id) {
+        if (!token) throw("Token not provided");
+        if (!client_id) throw("Client Id not provided");
+        if (!guild_id) throw("Guild Id not provided");
+
+        this.rest = new REST({ version: '9' }).setToken(token);
+        this.client_id = client_id;
+        this.guild_id = guild_id;
+    }
+
+    set_commands (commands) {
+        if (!commands) throw("No commands provided")
+
+        this.commands = commands.map(command => command.toJSON());
+    }
+
+    set_up () {
+        if (!this.commands) return;
+
+        this.rest.get(Routes.applicationGuildCommands(this.client_id, this.guild_id))
+            .then(async response => {
+                await response.forEach(async command => {
+                    await this.rest.delete(Routes.applicationGuildCommand(this.client_id, this.guild_id, command.id));
+                });
+                await this.rest.put(Routes.applicationGuildCommands(this.client_id, this.guild_id), { body: this.commands })
+                    .then(() => console.log(chalk.green('Successfully registered application commands.')))
+                    .catch(err => console.log(chalk.bgRed.whiteBright(err)));
+            });
+    }
+};
